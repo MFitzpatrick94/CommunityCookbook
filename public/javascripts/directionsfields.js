ca8419df9d8bfbe732b09cf4aa428205418cb638
MFitzpatrick94 <rasberrysoc@hotmail.com>
2018-06-18 10:59:02 -0500

    post form

--- public/javascripts/directionsfields.js
+++ public/javascripts/directionsfields.js
@@ -7,25 +7,25 @@ var count=4;
     count++;
 });*/
 
-$("#addParameter").click(function(){
+$("#addDirection").click(function(){
     //var html="<div id='div" + count + "'>";
     var html="<div id='div"+count+"'><input type='text' size='3' id='add" + count + "'></input></div>";
-    for (var i = 4; i < types.length; i++)
+   /* for (var i = 4; i < types.length; i++)
     {
         html += "<option>";
         html += types[i];
         html +=" </option>";
-    }
+    }*/
     html +="</select> ";
-    html += "<div id='div"+count+"'><input type='textarea' cols="50" rows="4" id='add"+ count +"'></input></div>";
+    html += "<div id='div"+count+"'><input type='textarea' cols='50' rows='4' id='add"+ count +"'></input></div>";
     //html += "<button type='button' onclick=remove('" + count + "')>Remove</button><br>";
     //html += </div>";
-    $("#testParameters").append(html);
+    $("#directions").append(html);
 
     count++;
 });
 
 /*function remove(which){
-  $("#div"+which).remove();
-}
+  $("#div"+which).remove();*/
+
 
