397513a589d3d34b5315ad26411bc1ddcac28351
MFitzpatrick94 <rasberrysoc@hotmail.com>
2018-07-06 14:43:47 -0500

    Community Cookbook

--- app/controllers/RecipeController.java
+++ app/controllers/RecipeController.java
@@ -62,27 +62,26 @@ public class RecipeController extends Controller
         addRecipe.setCategoryId(categoryId);
         addRecipe.setAuthor(authorName);
         addRecipe.setCookTime(cookTime);
-        if(file !=null && file.length()>0)
+        if (file != null && file.length() > 0)
         {
             try
             {
                 addRecipe.setPicture(Files.toByteArray(file));
-            }
-            catch (Exception e)
+            } catch (Exception e)
             {
                 //do nothing
             }
         }
         jpaApi.em().persist(addRecipe);
 
-        for(String formName : formNames)
+        for (String formName : formNames)
         {
-            if(formName.startsWith("amountId"))
+            if (formName.startsWith("amountId"))
             {
-                String rowNumber = formName.replace("amountId","");
-                String amount= form.get(formName);
+                String rowNumber = formName.replace("amountId", "");
+                String amount = form.get(formName);
                 int amountsId = Integer.parseInt(amount);
-                String ingredientName= form.get("ingredientName" + rowNumber);
+                String ingredientName = form.get("ingredientName" + rowNumber);
                 String measurement = form.get("measurementId" + rowNumber);
                 int measurements = Integer.parseInt(measurement);
 
@@ -96,9 +95,9 @@ public class RecipeController extends Controller
         }
 
 
-        for(String formName : formNames)
+        for (String formName : formNames)
         {
-            if(formName.startsWith("stepNumber"))
+            if (formName.startsWith("stepNumber"))
             {
                 String rowNumber = formName.replace("stepNumber", "");
                 String stepNumber = form.get(formName);
@@ -112,52 +111,53 @@ public class RecipeController extends Controller
                 directions.setRecipeId(addRecipe.getRecipeId());
 
 
-                jpaApi.em().persist(directions);
+                    jpaApi.em().persist(directions);
+                }
             }
+
+            return redirect(routes.CategoryController.getCategory());
+
+    }
+        @Transactional(readOnly = true)
+        public Result getRecipeDetails (Integer recipeId)
+        {
+            String sql = "SELECT r FROM Recipe r WHERE recipeId = :recipeId";
+
+            Recipe recipe = jpaApi.em().
+                    createQuery(sql, Recipe.class).
+                    setParameter("recipeId", recipeId).
+                    getSingleResult();
+
+            String ingredientSQl = "SELECT NEW models.IngredientMeasurement(i.ingredientId, i.ingredientName, a.amountName, m.measurementName)" +
+                    "FROM Ingredient i " +
+                    "JOIN Amount a on i.amountId = a.amountId " +
+                    "JOIN Measurement m on i.measurementId = m. measurementId " +
+                    " WHERE recipeId = :recipeId ORDER BY i.ingredientId";
+
+            List<IngredientMeasurement> ingredient = jpaApi.em().
+                    createQuery(ingredientSQl, IngredientMeasurement.class).
+                    setParameter("recipeId", recipeId).getResultList();
+
+            String directionSQL = "SELECT d FROM Directions d WHERE recipeId = :recipeId ORDER BY d.stepNumber";
+
+            List<Directions> directions = jpaApi.em().
+                    createQuery(directionSQL, Directions.class).
+                    setParameter("recipeId", recipeId).getResultList();
+
+
+            return ok(views.html.recipedisplay.render(recipe, ingredient, directions));
         }
 
-        return redirect(routes.CategoryController.getCategory());
-    }
+        @Transactional(readOnly = true)
+        public Result getPicture (Integer recipeId)
+        {
+            String sql = "SELECT r FROM Recipe r " +
+                    "WHERE recipeId = :recipeId";
 
-    @Transactional(readOnly = true)
-    public Result getRecipeDetails(Integer recipeId)
-    {
-        String sql = "SELECT r FROM Recipe r WHERE recipeId = :recipeId";
+            Recipe recipe = jpaApi.em().createQuery(sql, Recipe.class).setParameter("recipeId", recipeId).getSingleResult();
+            return ok(recipe.getPicture()).as("image/jpg");
+        }
 
-        Recipe recipe =  jpaApi.em().
-                createQuery(sql, Recipe.class).
-                setParameter("recipeId", recipeId).
-                getSingleResult();
-
-        String ingredientSQl = "SELECT NEW models.IngredientMeasurement(i.ingredientId, i.ingredientName, a.amountName, m.measurementName)" +
-                "FROM Ingredient i " +
-                "JOIN Amount a on i.amountId = a.amountId " +
-                "JOIN Measurement m on i.measurementId = m. measurementId " +
-                " WHERE recipeId = :recipeId ORDER BY i.ingredientId";
-
-        List<IngredientMeasurement> ingredient = jpaApi.em().
-                createQuery(ingredientSQl, IngredientMeasurement.class).
-                setParameter("recipeId", recipeId).getResultList();
-
-        String directionSQL = "SELECT d FROM Directions d WHERE recipeId = :recipeId ORDER BY d.stepNumber";
-
-        List<Directions> directions = jpaApi.em().
-                createQuery(directionSQL, Directions.class).
-                setParameter("recipeId", recipeId).getResultList();
-
-
-        return ok(views.html.recipedisplay.render(recipe, ingredient, directions));
-    }
-
-    @Transactional(readOnly = true)
-    public Result getPicture(Integer recipeId)
-    {
-        String sql = "SELECT r FROM Recipe r " +
-                "WHERE recipeId = :recipeId";
-
-        Recipe recipe = jpaApi.em().createQuery(sql, Recipe.class).setParameter("recipeId", recipeId).getSingleResult();
-        return ok(recipe.getPicture()).as("image/jpg");
-    }
 }
 
 
